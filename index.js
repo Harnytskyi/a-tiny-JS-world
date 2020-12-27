79e8c1213bc8699d43276d3d302c5432e45cf947
Harnytskyi <andrij.garnytskiy@gmail.com>
2020-12-27 21:09:50 +0200

    added tiny js world

--- index.js
+++ index.js
@@ -1,13 +1,72 @@
 /* Refer to https://github.com/OleksiyRudenko/a-tiny-JS-world for the task details
    Complete the below for code reviewers' convenience:
 
-   Code repository: _put repo URL here_
-   Web app: _put project's github pages URL here_
+   Code repository: https://harnytskyi.github.io/a-tiny-JS-world
+   Web app: https://github.com/Harnytskyi/a-tiny-JS-world
    */
 
 // ======== OBJECTS DEFINITIONS ========
-// Define your objects here
+class Inhabitant {
+    constructor(species, name, gender, legs, hands, saying) {
+        this.species = species;
+        this.name = name;
+        this.gender = gender;
+        this.legs = legs;
+        this.hands = hands;
+        this.saying = saying;
+    };
+    toString() {
+        const infoMessage = [
+            this.saying,
+            'I am a',
+            this.species + '.',
+            'My name is',
+            this.name + '.',
+            'My gender is',
+            this.gender + '.',
+            `I have`,
+            this.legs,
+            this.definitionExtremities()
+        ];
+        return infoMessage.join(' ');
+    }
+    definitionExtremities() {
+        if (this.species == 'human')
+            return 'legs and ' + this.hands + ' hands.'
+        else
+            return 'paws.'
+    }
+}
+class Human extends Inhabitant {
+    constructor(name, gender, saying) {
+        super('human', name, gender, 2, 2, saying);
+    }
+}
+class Man extends Human {
+    constructor(name, saying) {
+        super(name, 'male', saying);
+    }
+}
+class Woman extends Human {
+    constructor(name, saying) {
+        super(name, 'female', saying);
+    }
+}
+class Dog extends Inhabitant {
+    constructor(name, gender) {
+        super('dog', name, gender, 4, 0, 'Gav');
+    }
+}
+class Cat extends Inhabitant {
+    constructor(name, gender) {
+        super('cat', name, gender, 4, 0, 'Meow');
+    }
+}
 
+const man = new Man('John', 'Hello, everybody!');
+const woman = new Woman('Jessica', 'Good morning, gays!');
+const cat = new Cat('Tom', 'male');
+const dog = new Dog('Jerry', 'female');
 
 // ======== OUTPUT ========
 /* Use print(message) for output.
@@ -18,14 +77,6 @@
    so code reviewers might focus on a single file that is index.js.
    */
 
-/* Print examples:
-   print('ABC');
-   print('<strong>ABC</strong>');
-   print('<strong>ABC</strong>', 'div');
-
-   print('human; John; male; 2; 2; Hello world!; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny');
-   print('human; <strong>John</strong>; male; 2; 2; <em>Hello world!</em>; Rex, Tom, Jenny', 'div');
-   */
-
+const inhabitants = [man, woman, cat, dog];
+inhabitants.forEach(key => print(key));
 
